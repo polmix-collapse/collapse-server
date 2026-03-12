8b659f6ca3e4167d8ae755087f5b7c27121792f4
polmix-collapse <abebus282@gmail.com>
2026-03-12 23:52:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -122,4 +122,5 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, '0.0.0.0', () => console.log(🚀 Сервер на порту ${PORT}));
+
+server.listen(PORT, '0.0.0.0', () => console.log("🚀 Сервер на порту ${PORT}");
